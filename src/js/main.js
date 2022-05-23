2f501decf0adaec43a8d4a58c6d13c6515620464
Hakan Bilgin <hbi99@hotmail.com>
2022-05-23 22:40:54 +0200

    first commit

---
+++ src/js/main.js
@@ -0,0 +1,18 @@
+
+const mail = {
+	init() {
+		// fast references
+		this.content = window.find("content");
+	},
+	dispatch(event) {
+		switch (event.type) {
+			case "window.init":
+				break;
+			case "open-help":
+				defiant.shell("fs -u '~/help/index.md'");
+				break;
+		}
+	}
+};
+
+window.exports = mail;
