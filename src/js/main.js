6d09710bd2f5bba935af32080674a6f506ba1a8b
Hakan Bilgin <hbi99@hotmail.com>
2022-08-13 23:05:46 +0200

    Update main.js

--- src/js/main.js
+++ src/js/main.js
@@ -9,13 +9,14 @@ const mail = {
 		// temp
 		// setTimeout(() => this.dispatch({ type: "new-mail" }), 250);
 		// setTimeout(() => $(".mail-body").focus(), 350);
-		// setTimeout(() => {
+		setTimeout(() => {
 			// let win = $(`.ant-window_[data-id="mail-new-mail"]`);
 			// el.trigger("mousedown");
 			// defiant_.window_.close_(win)
 
 			// window.close();
-		// }, 600);
+			window.focus();
+		}, 600);
 	},
 	dispatch(event) {
 		let Self = mail,
